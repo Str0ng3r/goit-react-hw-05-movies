1efee779030c871c1feec52c64af0fd49992c220
Str0ng3r <116167012+Str0ng3r@users.noreply.github.com>
2023-05-31 21:44:39 +0200

    UPDATE

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,10 +1,20 @@
 import React, { useState } from 'react';
-import { Link} from 'react-router-dom';
+import { Link,useSearchParams} from 'react-router-dom';
 import styled from './styled.module.css';
 
 export const Movies = () => {
   const [search, setSearch] = useState('');
   const [films, setFilms] = useState(null);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const name = searchParams.get("name");
+
+
+const funcInput = (evt) => {
+  setSearch(evt.target.value);
+}
+
+
+
   const options = {
     method: 'GET',
     headers: {
@@ -15,6 +25,7 @@ export const Movies = () => {
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
+    setSearchParams({ name: search})
     fetch(
       `https://api.themoviedb.org/3/search/movie?query=${search}&include_adult=false&language=en-US&page=1`,
       options
@@ -32,7 +43,7 @@ export const Movies = () => {
             className={styled.input}
             type="text"
             value={search}
-            onChange={(evt) => setSearch(evt.target.value)}
+            onChange={funcInput}
           />
           <button type="submit">Search</button>
         </form>
